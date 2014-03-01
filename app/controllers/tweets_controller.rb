fead09ca0381cd526a7f75c48f43a53c6bbdeec8
avjgit <avjmail@gmail.com>
2014-03-01 10:56:43 +0000

    new tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,8 +4,15 @@ class TweetsController < ApplicationController
   end
   
   def new
+    @tweet = Tweet.new
   end
   
   def create
+    @tweet = Tweet.new(params.require(:tweet).permit(:message).merge(user: current_user))
+    if @tweet.save
+      redirect_to action: 'index', notice: 'Tweet created!'
+    else
+      render :new
+    end
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+  validates :message, presence: true, length: {in: 3..500}
 end
