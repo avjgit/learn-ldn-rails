0213c60e419bdb914aef7ed285d0e4b3a53bc190
avjgit <avjmail@gmail.com>
2014-03-01 10:38:18 +0000

    tweets resources; links in sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
+  
+  rails_admin do
+    edit do
+      field :email
+      field :password
+      field :password_confirmation
+      field :tweets
+    end
+  end
 end
